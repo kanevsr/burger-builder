333a3728549ffd7495a5338c17d3230d8a24a27a
Ljupcho Kanevche <ljupcho.kanevche@gmail.com>
2019-11-21 15:20:43 +0100

    Do negde pak

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,16 +23,19 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
 };
 
-export const checkAuthTimeout = (expirationTime) => {
+export const checkAuthTimeout = (expirationDate) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime) 
+        }, expirationDate) 
     }
 } 
 
@@ -51,6 +54,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
         .then( response => {
             console.log(response);
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('token', response.data.idToken)
+            localStorage.setItem('expirationDate', response.data.idToken)
+            localStorage.setItem('userId', response.data.localId)
             dispatch(authSuccess(response.data.idToken, response.data.localId))
             dispatch(checkAuthTimeout(response.data.expiresIn));
         })
@@ -64,5 +71,23 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if(expirationDate > new Date()){
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+            } else {
+                dispatch(logout());
+            }
+        }
     }
 }
